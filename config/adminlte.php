8af6804e6da7ad6c4478c27ec6777e84b6877b8c
osneidabordones <osneidabordones@gmail.com>
2021-10-13 11:04:38 -0400

    plantilla adminlte

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => '| Osneida',
+    'title' => 'AdminLTE 3',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -45,12 +45,12 @@ return [
     |
     */
 
-    'logo' => '<b>Osneida</b>Bordones',
+    'logo' => '<b>Admin</b>LTE',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'Osneida',
+    'logo_img_alt' => 'AdminLTE',
 
     /*
     |--------------------------------------------------------------------------
@@ -188,7 +188,7 @@ return [
     */
 
     'use_route_url' => false,
-    'dashboard_url' => 'dashboard',
+    'dashboard_url' => 'home',
     'logout_url' => 'logout',
     'login_url' => 'login',
     'register_url' => 'register',
@@ -247,96 +247,78 @@ return [
             'can'  => 'manage-blog',
         ],
         [
-            'text'        => 'Dashboard',
-            'route'       => 'admin',
-            'icon'        => 'fas fa-tachometer-alt',
+            'text'        => 'pages',
+            'url'         => 'admin/pages',
+            'icon'        => 'far fa-fw fa-file',
+            'label'       => 4,
             'label_color' => 'success',
-            'can'         => 'admin.home'
+        ],
+        ['header' => 'account_settings'],
+        [
+            'text' => 'profile',
+            'url'  => 'admin/settings',
+            'icon' => 'fas fa-fw fa-user',
         ],
         [
-            'text'        => 'Usuarios',
-            'route'       => 'admin.users.index',
-            'icon'        => 'fas fa-users fa-fw',
-            'label_color' => 'success',
-            'can'         => 'admin.users.index',
+            'text' => 'change_password',
+            'url'  => 'admin/settings',
+            'icon' => 'fas fa-fw fa-lock',
         ],
         [
-            'text'        => 'Roles',
-            'route'       => 'admin.roles.index',
-            'icon'        => 'fas fa-users-cog fa-fw',
-            'can'         => 'admin.users.index',
+            'text'    => 'multilevel',
+            'icon'    => 'fas fa-fw fa-share',
+            'submenu' => [
+                [
+                    'text' => 'level_one',
+                    'url'  => '#',
+                ],
+                [
+                    'text'    => 'level_one',
+                    'url'     => '#',
+                    'submenu' => [
+                        [
+                            'text' => 'level_two',
+                            'url'  => '#',
+                        ],
+                        [
+                            'text'    => 'level_two',
+                            'url'     => '#',
+                            'submenu' => [
+                                [
+                                    'text' => 'level_three',
+                                    'url'  => '#',
+                                ],
+                                [
+                                    'text' => 'level_three',
+                                    'url'  => '#',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                [
+                    'text' => 'level_one',
+                    'url'  => '#',
+                ],
+            ],
         ],
-        [   'header' => 'ADMINISTRADOR',
-            'can'         => 'admin.users.index',
+        ['header' => 'labels'],
+        [
+            'text'       => 'important',
+            'icon_color' => 'red',
+            'url'        => '#',
         ],
         [
-            'text'  => 'Categorías',
-            'route' => 'admin.categories.index', //http://apibase/admin/categories
-            'icon'  => 'fab fa-battle-net',
-          //  'active'=> ['admin/categories*'],
-            'can'   => 'admin.categories.index',
-            
-        ],
-
-        ['header' => 'OPCIONES DE BLOGER'],
-        
-        [
-            'text'  => 'Lista de post',
-            'route' => 'admin.posts.index',  
-            'icon'  => 'fab fa-ioxhost',
-            'can'   => 'admin.posts.index'
+            'text'       => 'warning',
+            'icon_color' => 'yellow',
+            'url'        => '#',
         ],
         [
-            'text'  => 'Crear nuevo post',
-            'route' => 'admin.posts.create', 
-            'icon'  => 'fas fa-book-reader',
-            'can'   => 'admin.posts.create', 
+            'text'       => 'information',
+            'icon_color' => 'cyan',
+            'url'        => '#',
         ],
-
-        // [
-        //     'text' => 'change_password',
-        //     'url'  => 'admin/settings',
-        //     'icon' => 'fas fa-fw fa-lock',
-        // ],
-        // [
-        //     'text'    => 'multilevel',
-        //     'icon'    => 'fas fa-fw fa-share',
-        //     'submenu' => [
-        //         [
-        //             'text' => 'level_one',
-        //             'url'  => '#',
-        //         ],
-        //         [
-        //             'text'    => 'level_one',
-        //             'url'     => '#',
-        //             'submenu' => [
-        //                 [
-        //                     'text' => 'level_two',
-        //                     'url'  => '#',
-        //                 ],
-        //                 [
-        //                     'text'    => 'level_two',
-        //                     'url'     => '#',
-        //                     'submenu' => [
-        //                         [
-        //                             'text' => 'level_three',
-        //                             'url'  => '#',
-        //                         ],
-        //                         [
-        //                             'text' => 'level_three',
-        //                             'url'  => '#',
-        //                         ],
-        //                     ],
-        //                 ],
-        //             ],
-        //         ],
-        //         [
-        //             'text' => 'level_one',
-        //             'url'  => '#',
-        //         ],
-        //     ],
-        // ],
-   ],
+    ],
 
     /*
     |--------------------------------------------------------------------------
@@ -490,6 +472,5 @@ return [
     |
     */
 
-    'livewire' => true,
-    
+    'livewire' => false,
 ];
